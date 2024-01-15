d352a35d1aa8d4b81a38d9db90d021a69239cc7d
Nishank Priydarshi <nishankpr2002@gmail.com>
2024-01-15 19:28:34 +0530

    added error handling in user_interaction service

--- content_service/src/utils/error/api-error.js
+++ content_service/src/utils/error/api-error.js
@@ -5,42 +5,42 @@ const HTTP_CODES = {
     UNPROCESSABLE_ENTITY: 422,
     NOT_FOUND:404,
     Unauthorized:401,
-  }
+}
   
-  class BaseError extends Error {
-      constructor(name, statuscode, description) {
-          super(description)
-          this.name=name
-          this.statuscode=statuscode
-          this.description=description
-          Error.captureStackTrace(Error)
-      }
-  }
+class BaseError extends Error {
+    constructor(name, statuscode, description) {
+        super(description)
+        this.name=name
+        this.statuscode=statuscode
+        this.description=description
+        Error.captureStackTrace(Error)
+    }
+}
+ 
+class Unauthorized extends BaseError {
+    constructor(description, name, statuscode=HTTP_CODES.Unauthorized) {
+        super(name="Unauthorized", statuscode, description)
+    }
+}
   
-  class Unauthorized extends BaseError {
-      constructor(description, name, statuscode=HTTP_CODES.Unauthorized) {
-          super(name="Unauthorized", statuscode, description=description)
-      }
-  }
+class BadRequest extends BaseError {
+    constructor(description, name, statuscode=HTTP_CODES.BAD_REQUEST){
+        super(name='BAD Request', statuscode, description)
+    }
+}
   
-  class BadRequest extends BaseError {
-      constructor(description, name, statuscode=HTTP_CODES.BAD_REQUEST){
-          super(name='BAD Request', statuscode, description=description)
-      }
-  }
+class NotFound extends BaseError {
+    constructor(description, name, statuscode=HTTP_CODES.NOT_FOUND){
+        super(name='Not Found', statuscode, description)
+    }
+}
   
-  class NotFound extends BaseError {
-      constructor(description, name, statuscode=HTTP_CODES.NOT_FOUND){
-          super(name='Not Found', statuscode, description=description)
-      }
-  }
+class INTERNAL extends BaseError {
+    constructor(description, name, statuscode=HTTP_CODES.INTERNAL_SERVER){
+        super(name='Internal Server Error', statuscode, description = 'Something Went Wrong')
+    }
+}
   
-  class INTERNAL extends BaseError {
-      constructor(description, name, statuscode=HTTP_CODES.INTERNAL_SERVER){
-          super(name='Internal Server Error', statuscode, description='Something Went Wrong')
-      }
-  }
-  
-  module.exports={
-      Unauthorized, BadRequest, NotFound, INTERNAL, BaseError
-  }
+module.exports={
+    Unauthorized, BadRequest, NotFound, INTERNAL, BaseError
+}
--- user_interaction_service/src/app.js
+++ user_interaction_service/src/app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 
 const { development } = require('./config')
+const { pageNotfound, ErrorHandler } = require('./utils/error/error-handler')
 
 const PORT = development.port || 6000 
 
 app.use(express.json())
 
 const main = async () => {
+    app.use(pageNotfound)
+    app.use(ErrorHandler)
+    
     const server = app.listen(PORT, () => {
         console.log(`user interaction service listening on port ${PORT}`)
     })
